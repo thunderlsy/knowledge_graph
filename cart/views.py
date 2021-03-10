85a241c72df7e4cef419c7113e2dfd0b1948c2b9
Chris <lishuyiplus@foxmail.com>
2021-03-10 09:09:00 +0800

    整理代码

--- cart/views.py
+++ cart/views.py
@@ -3,6 +3,31 @@
 
 from cart import cart_bp
 from flask_restful import Resource, Api
+from flask_restful.representations import json
+
+cart_api = Api(cart_bp)
+
+
+# 通用装饰器
+def logging1(func):
+    print("logging_1装饰器外部")
+
+    def inner(*args, **kwargs):
+        print("logging_1装饰器内部")
+        return func(*args, **kwargs)
+
+    return inner
+
+
+# 通用装饰器
+def logging2(func):
+    print("logging_2装饰器外部")
+
+    def inner(*args, **kwargs):
+        print("logging_2装饰器内部")
+        return func(*args, **kwargs)
+
+    return inner
 
 
 @cart_bp.route('/info', methods=["GET"])
@@ -22,6 +47,10 @@ class UserResource1(Resource):
 
 # 可以添加多个类视图
 class UserResource2(Resource):
+    method_decorators = {
+        'get': [logging1, logging2],
+        'post': [logging2]
+    }
 
     def get(self):
         return "get user"
@@ -30,6 +59,5 @@ class UserResource2(Resource):
         return "post user"
 
 
-cart_api = Api(cart_bp)
 cart_api.add_resource(UserResource1, '/info1', endpoint="cart_route1")
 cart_api.add_resource(UserResource2, '/info2', endpoint="cart_route2")
--- search/views.py
+++ search/views.py
@@ -5,41 +5,7 @@ from flask import jsonify, current_app
 from search import search_bp
 from flask_restful import Resource, Api
 
-
-# @search_bp.route('/<node_name>', methods=["GET"])
-# def search(node_name):
-#     # 二级查询
-#     # gql = "match (start_node)-[relationship]-(end_node) where start_node.name = '{}' return start_node,relationship,end_node".format(node_name)
-#
-#     # 三级查询
-#     gql = "match (start_node)-[first_relationship]->(second_node) where start_node.name = '{}' WITH start_node,first_relationship,second_node match (second_node)-[second_relationship]->(third_node) return start_node,first_relationship,second_node,second_relationship,third_node".format(
-#         node_name)
-#
-#     sub_graph = current_app.graph.run(gql).data()
-#
-#     if not sub_graph:
-#         json_dict = {
-#             "error_code": 1,
-#             "data": "数据库查无此数据",
-#         }
-#         return jsonify(json_dict)
-#
-#     relationship_end = []
-#
-#     for i in sub_graph:
-#
-#         if i['second_node']['id'] != i['third_node']['id']:
-#             relationship_end.append({"source_name": i['second_node']['name'], "source_id": i['second_node']['id'],
-#                                      "relationship": type(i['second_relationship']).__name__,
-#                                      "target_name": i['third_node']['name'], "target_id": i['third_node']['id']})
-#
-#     json_dict = {
-#         "error_code": 0,
-#         "start_node": {"start_name": node_name, "start_id": sub_graph[0]['start_node']['id']},
-#         "relationship_information": relationship_end,
-#     }
-#
-#     return jsonify(json_dict)
+search_api = Api(search_bp)
 
 
 class ThreeLevelSearch(Resource):
@@ -74,8 +40,8 @@ class ThreeLevelSearch(Resource):
             "relationship_information": relationship_end,
         }
 
-        return json_dict
+        return jsonify(json_dict)
+        # return json_dict
 
 
-search_api = Api(search_bp)
 search_api.add_resource(ThreeLevelSearch, '/<node_name>', endpoint="Three_Level_Search")
